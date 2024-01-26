0fbb644e27a08a77d5e28915a76e7139ffd0a742
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2024-01-26 17:21:24 +0700

    3 get api test, n template get
    get all dct, get all help cat by type, get all help type

---
+++ support/components/generate.js
@@ -0,0 +1,9 @@
+export const  generateAuthToken= (type) => {
+    let res = ""
+
+    if(type == "hardcode"){
+        res = "1262|AtuiWKBRrDdMljzc9jDLVgpuewSKmM7QGUXZHNRF"
+    }
+
+    return res
+}
---
+++ support/integrations/get_api/get_system.js
@@ -0,0 +1,44 @@
+// Components
+import { generateAuthToken } from '../../components/generate'
+import '../../components/template'
+
+describe('MI-FIK API Testing - Help', () => {
+    // Template
+    const method = 'get'
+    const token = generateAuthToken("hardcode")
+
+    // =============== DICTIONARY MODULE ===============
+    // Public API
+    it(method.toUpperCase() + ' - All Dictionary', () => {
+        const typeDct = 'TAG-001'
+
+        cy.request('api/v1/dictionaries/type/'+typeDct).as(method + 'AllDictionary')
+        cy.get('@' + method + 'AllDictionary').then(sys => {
+            cy.templateGet(sys, false)
+        })
+    })
+
+    // ================== HELP MODULE ==================
+
+    // Public API
+    it(method.toUpperCase() + ' - All Help Category By Type', () => {
+        cy.request('api/v1/help/event').as(method + 'AllHelpCategoryByType')
+        cy.get('@' + method + 'AllHelpCategoryByType').then(sys => {
+            cy.templateGet(sys, true)
+        })
+    })
+
+    // Private API
+    it(method.toUpperCase() + ' - All Help Type', () => {
+        cy.request({
+            method: method,
+            url: 'api/v1/help',
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).as(method + 'AllHelpType')
+        cy.get('@' + method + 'AllHelpType').then(sys => {
+            cy.templateGet(sys, false)
+        })
+    })
+})
